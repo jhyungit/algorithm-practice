e8bc42ccb7649bab8564b7532bb491b9cbc27169
jhyungit <92933743+jhyungit@users.noreply.github.com>
2026-04-08 10:50:39 +0900

    [Gold V] Title: 평범한 배낭, Time: 1724 ms, Memory: 228328 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.py
@@ -1,13 +1,26 @@
+import sys
+input = sys.stdin.readline
+
+def solution():
+    dp = [[0] * (k+1) for _ in range(n + 1)]
+
+    # max(바로 위, 현재 아이템 가치)
+    for item in range(1, n + 1): # 선택 가능한 아이템 범위
+        weight, value = products[item]
+        for limit in range(k + 1):
+            # 아이템 무게 <= 제한 무게이면
+            if weight <= limit:
+                dp[item][limit] = max(dp[item-1][limit], dp[item-1][limit - weight] + value)
+            else:
+                dp[item][limit] = dp[item-1][limit]
+    
+    print(dp[n][k])
+
+# 물건 N개, 무게 W, 가치 V
+# 제한 무게 K
 n, k = map(int, input().split())
-items = [tuple(map(int, input().split())) for _ in range(n)]
-dp = [[0] * (k + 1) for _ in range(n + 1)]
-
-for i in range(1, n + 1):
-    w, v = items[i - 1]
-    for j in range(k + 1):
-        if j < w:
-            dp[i][j] = dp[i - 1][j]
-        else:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - w] + v)
-
-print(dp[n][k])
+products = [None]
+for _ in range(n):
+    w, v= map(int, input().split())
+    products.append((w,v))
+solution()
